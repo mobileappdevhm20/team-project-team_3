9a98475b6e21973b906fba27bcecf54ab0c9b800
David Haupenthal <david.haupenthal@hotmail.de>
2020-04-24 20:03:26 +0200

    app init

---
+++ app/src/main/java/team3/recipefinder/MainActivity.kt
@@ -0,0 +1,18 @@
+package team3.recipefinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import team3.recipefinder.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
