e6d15178b4a6824bca57502e6f4f370ea2795d27
Jan Bremauer <bremauer.jan2@gmail.com>
2020-05-16 13:18:31 +0200

    Added material toolbar with menu

--- app/src/main/java/team3/recipefinder/MainActivity.kt
+++ app/src/main/java/team3/recipefinder/MainActivity.kt
@@ -1,12 +1,66 @@
 package team3.recipefinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
+import team3.recipefinder.activity.LoginActivity
+import team3.recipefinder.R
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var logoutBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+
+        if(auth.currentUser == null){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            Toast.makeText(this, "Already logged in", Toast.LENGTH_LONG).show()
+        }
+
         setContentView(R.layout.main_activity)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        val mainToolbar: Toolbar = findViewById(R.id.toolbar)
+        val mainMenu: Menu = mainToolbar.menu
+        val userNameItem: MenuItem = mainMenu.findItem(R.id.user_name_settings)
+
+        userNameItem.title = FirebaseAuth.getInstance().currentUser?.email
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.user_logout_settings -> {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(this, "Successfully Logged Out", Toast.LENGTH_LONG).show()
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
