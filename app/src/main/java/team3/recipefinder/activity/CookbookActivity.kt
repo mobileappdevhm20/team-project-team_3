ceef2cfc710775f4120c4d08201a298d225ea4f8
Matthias Rupp <matthias.rupp@posteo.de>
2020-05-29 11:00:45 +0200

    Add Recipe list

---
+++ app/src/main/java/team3/recipefinder/activity/CookbookActivity.kt
@@ -0,0 +1,30 @@
+package team3.recipefinder.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_cookbook.*
+import team3.recipefinder.R
+import team3.recipefinder.adapter.RecipeListAdapter
+import team3.recipefinder.model.Recipe
+
+class CookbookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cookbook)
+
+        val mockedRecipeData = listOf(
+            Recipe(1, "Cake"),
+            Recipe(2, "Tomato Soup"),
+            Recipe(3, "Beer soup")
+        )
+
+        recipeList.layoutManager = LinearLayoutManager(this)
+
+        val recipeListAdapter = RecipeListAdapter(this)
+        recipeList.adapter = recipeListAdapter
+        recipeListAdapter.recipes.addAll(mockedRecipeData)
+        recipeListAdapter.notifyDataSetChanged()
+    }
+}
