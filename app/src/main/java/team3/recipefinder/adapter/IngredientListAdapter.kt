529d15b69b5fc585f927fbe37bfa5e0d27391970
Jan Bremauer <bremauer.jan2@gmail.com>
2020-05-31 13:42:23 +0200

    Added onclickListener for listview items

--- app/src/main/java/team3/recipefinder/adapter/IngredientListAdapter.kt
+++ app/src/main/java/team3/recipefinder/adapter/IngredientListAdapter.kt
@@ -1,6 +1,8 @@
 package team3.recipefinder.adapter
 
 import android.content.Context
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +10,10 @@ import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
 import team3.recipefinder.R
+import team3.recipefinder.activity.RecipeDetailActivity
+import team3.recipefinder.dialog.EditIngredientFragment
 
-class IngredientListAdapter(context: Context, private val ingredientNames: List<String>, private val editMode: Boolean) : ArrayAdapter<String>(context, R.layout.ingredient_list_item,  ingredientNames){
+class IngredientListAdapter(private val inputContext: Context, private val ingredientNames: List<String>, private val ingredientIds: List<Long>, private val editMode: Boolean) : ArrayAdapter<String>(inputContext, R.layout.ingredient_list_item,  ingredientNames){
 
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         val inflater: LayoutInflater = LayoutInflater.from(context)
@@ -19,15 +23,27 @@ class IngredientListAdapter(context: Context, private val ingredientNames: List<
         val name = rowView.findViewById(R.id.ingredient_item__name) as TextView
         val editImageView = rowView.findViewById(R.id.edit_ingredient) as ImageView
 
-        amount.text = "5 tbs"
-        name.text = ingredientNames[position]
+        val currentId = ingredientIds[position]
+        val currentIngredient = ingredientNames[position]
+        val currentAmount = "5 tbs"
+
+        amount.text = currentAmount
+        name.text = currentIngredient
 
         if(editMode) {
             editImageView.visibility = View.VISIBLE
+            rowView.setOnClickListener(View.OnClickListener {
+                Log.i("IngredientListAdapter", "Editing with id = $currentId and ingredient = $currentIngredient")
+                (inputContext as RecipeDetailActivity).showEditIngredients(currentId, currentIngredient, currentAmount)
+            })
         } else {
             editImageView.visibility = View.GONE
         }
 
         return rowView
+    }
+
+    override fun getItem(position: Int): String? {
+        return ingredientNames[position]
     }
 }
