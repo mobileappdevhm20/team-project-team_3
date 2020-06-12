9a82a82bd0a9b71c409abed53fc20789495fa94c
David <haupenth@hm.edu>
2020-06-12 12:50:27 +0200

    add regex for amount

--- app/src/main/java/team3/recipefinder/adapter/IngredientListAdapter.kt
+++ app/src/main/java/team3/recipefinder/adapter/IngredientListAdapter.kt
@@ -1,7 +1,6 @@
 package team3.recipefinder.adapter
 
 import android.content.Context
-import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +10,18 @@ import android.widget.ImageView
 import android.widget.TextView
 import team3.recipefinder.R
 import team3.recipefinder.activity.RecipeDetailActivity
-import team3.recipefinder.dialog.EditIngredientFragment
+import java.util.regex.Pattern
 
 /**
  * Custom adapter to display the ingredients in a custom listview.
  */
-class IngredientListAdapter(private val inputContext: Context, private val ingredientNames: List<String>, private val ingredientAmounts: List<String>, private val relationIds: List<Long>, private val editMode: Boolean) : ArrayAdapter<String>(inputContext, R.layout.ingredient_list_item,  ingredientNames){
-
+class IngredientListAdapter(
+    private val inputContext: Context,
+    private val ingredientNames: List<String>,
+    private val ingredientAmounts: List<String>,
+    private val relationIds: List<Long>,
+    private val editMode: Boolean
+) : ArrayAdapter<String>(inputContext, R.layout.ingredient_list_item, ingredientNames) {
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         val inflater: LayoutInflater = LayoutInflater.from(context)
         val rowView = inflater.inflate(R.layout.ingredient_list_item, null, true)
@@ -30,21 +34,42 @@ class IngredientListAdapter(private val inputContext: Context, private val ingre
         val currentIngredient = ingredientNames[position]
         val currentAmount = ingredientAmounts[position]
 
-
-        amount.text = currentAmount
+        amount.text = calculateAmount(currentAmount)
         name.text = currentIngredient
 
-        if(editMode) {
+        if (editMode) {
             editImageView.visibility = View.VISIBLE
             rowView.setOnClickListener(View.OnClickListener {
-                Log.i("IngredientListAdapter", "Editing with id = $currentId and ingredient = $currentIngredient")
-                (inputContext as RecipeDetailActivity).showEditIngredients(currentId, currentIngredient, currentAmount)
+                Log.i(
+                    "IngredientListAdapter",
+                    "Editing with id = $currentId and ingredient = $currentIngredient"
+                )
+                (inputContext as RecipeDetailActivity).showEditIngredients(
+                    currentId,
+                    currentIngredient,
+                    currentAmount
+                )
             })
         } else {
             editImageView.visibility = View.GONE
         }
 
         return rowView
+    }
+
+    fun calculateAmount(value: String): String {
+        val returnValue: String
+        val factor = 2
+        val regex = "(\\d*\\.\\d+)|(\\d+\\.?)"
+        val m = Pattern.compile(regex).matcher(value)
+        if (m.find()) {
+            val number = m.group()
+            returnValue =
+                value.replace(number, (java.lang.Float.valueOf(number) * factor).toString())
+        } else {
+            returnValue = value
+        }
+        return returnValue
     }
 
     override fun getItem(position: Int): String? {
