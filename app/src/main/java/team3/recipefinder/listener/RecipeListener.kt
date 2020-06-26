9e2cbae088b97ce03f2f73b2d2d1466427ecdeeb
David <haupenth@hm.edu>
2020-06-26 14:57:44 +0200

    removed unnecessary files

--- app/src/main/java/team3/recipefinder/listener/RecipeListener.kt
+++
@@ -1,8 +0,0 @@
-package team3.recipefinder.listener
-
-import team3.recipefinder.model.Recipe
-import team3.recipefinder.viewmodel.RecipeViewModel
-
-class RecipeListener(private val viewModel: RecipeViewModel) {
-    fun onEdit(t: Recipe) = viewModel.editPropertyDetails(t)
-}
--- app/src/main/java/team3/recipefinder/viewmodel/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package team3.recipefinder.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
